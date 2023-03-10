a27f7b469356627dc47723fa4f7fbf5ec3d7e133
MihaiMalos <malosmihai1@yahoo.com>
2023-03-10 10:31:44 +0200

    Add files via upload

---
+++ events/reconnecting.js
@@ -0,0 +1,7 @@
+module.exports = client => { // eslint-disable-line no-unused-vars
+  console.log(`Reconnecting at ${new Date()}`);
+ // var sys = require('sys')
+//  var exec = require('child_process').exec;
+//  function puts(error, stdout, stderr) { message.channel.sendmessage("Executat!") }
+//  exec("pm2 restart app.js", puts);
+};
